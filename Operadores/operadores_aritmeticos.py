2500c582c469d66be78a882a67cc5907c2670ced
Eduardo <eduardo25082002@gmail.com>
2026-03-13 10:25:29 -0300

    Quarto Commit

---
+++ Operadores/operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+# Adição
+print(5 + 5)
+
+# Subtração
+print(7 - 2)
+
+# Multiplicação
+print(10 * 2)
+
+# Divisão
+print(10 / 2)
+
+# Divisão Inteira
+print(15 // 3)
+
+# Módulo
+print(10 % 3)
+
+# Exponenciação
+print(2 ** 3)
---
+++ Operadores/precedencia.py
@@ -0,0 +1,20 @@
+# Ordem de Precedência dos Operadores
+
+# 1. Parenteses ()
+# 2. Expoentes **
+# 3. Multip e Divs * e /, // -> Esquerda p/ direita
+# 4. Adição e Subs + e - -> Esquerda p/ direita
+
+######################################
+
+# Exemplos
+
+print(10 - 5 * 2) # Direita p/ esquerda
+
+print((10 - 5) * 2) # Esquerda p/ direita
+
+print(10 ** 2 * 2) # Esquedra p/ direita
+
+print(10 ** (2 * 2)) # Direita p/ esquerda
+
+print(10 / 2 * 4) # Esquerda p/ direita
